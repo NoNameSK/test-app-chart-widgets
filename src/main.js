9176fb8b089e445830d4c38811d497762172cbda
NoNameSK <55396512+NoNameSK@users.noreply.github.com>
2023-07-07 03:01:04 +0600

    init deploy build

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import HighchartsVue from 'highcharts-vue'
-import store from './store'
-import './assets/styles.css'
-
-createApp(App).use(store).use(HighchartsVue).mount('#app')
